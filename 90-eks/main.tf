9de2ac2c1c094dfe2e58e27bbbd5e7e26f04a60e
Prav-Akula <pravs.akula@outlook.com>
2026-03-10 20:20:39 -0400

    app-blue-green

--- 90-eks/main.tf
+++ 90-eks/main.tf
@@ -31,20 +31,20 @@ module "eks" {
 
   # EKS Managed Node Group(s)
   eks_managed_node_groups = {
-    # blue = {
-    # #   create = var.enable_blue
-    #   ami_type       = "AL2023_x86_64_STANDARD"
-    # #   kubernetes_version = var.eks_nodegroup_blue_version
-    #   instance_types = ["t3.small"]
-    #   iam_role_additional_policies  = {
-    #     amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-    #     amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-    #   }
+    blue = {
+    #   create = var.enable_blue
+      ami_type       = "AL2023_x86_64_STANDARD"
+    #   kubernetes_version = var.eks_nodegroup_blue_version
+      instance_types = ["t3.small"]
+      iam_role_additional_policies  = {
+        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+      }
       
-    #   # cluster nodes autoscaling
-    #   min_size     = 2
-    #   max_size     = 10
-    #   desired_size = 2
+      # cluster nodes autoscaling
+      min_size     = 2
+      max_size     = 10
+      desired_size = 2
 
     # #   # taints = {
     # #   #   upgrade = {
@@ -57,22 +57,22 @@ module "eks" {
     # # #   labels = {
     # # #     nodegroup = "blue"
     # # #   }
-    # }
+    }
 
-    green = {
-    #   create = var.enable_green
-      ami_type       = "AL2023_x86_64_STANDARD"
-    #   kubernetes_version = var.eks_nodegroup_green_version
-      instance_types = ["t3.small"]
-      iam_role_additional_policies  = {
-        amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
-        amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-      }
+    # green = {
+    # #   create = var.enable_green
+    #   ami_type       = "AL2023_x86_64_STANDARD"
+    # #   kubernetes_version = var.eks_nodegroup_green_version
+    #   instance_types = ["t3.small"]
+    #   iam_role_additional_policies  = {
+    #     amazonEFS = "arn:aws:iam::aws:policy/service-role/AmazonEFSCSIDriverPolicy"
+    #     amazonEBS = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
+    #   }
       
-      # cluster nodes autoscaling
-      min_size     = 2
-      max_size     = 10
-      desired_size = 2
+    #   # cluster nodes autoscaling
+    #   min_size     = 2
+    #   max_size     = 10
+    #   desired_size = 2
 
       # taints = {
       #   upgrade = {
@@ -87,8 +87,8 @@ module "eks" {
     # #   }
     # }
   }  
- }
- 
+#  }
+
  tags = merge(
     local.common_tags,
     {
